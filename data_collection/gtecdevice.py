f4bb33b62f5538bfdfb8cec4a83e6b285c4f73a2
kyosukemine <k3.mine.0131@gmail.com>
2022-12-15 19:40:37 +0900

    ADD todo

--- data_collection/gtecdevice.py
+++ data_collection/gtecdevice.py
@@ -98,6 +98,7 @@ class InputStream():
                 _data = [[] for _ in range(self.channels)]
                 # print(rcv_data)
                 # print(*struct.iter_unpacpk('<f', rcv_data))
+                # TODO データが全て送られているかの保証がないためgtec側のプログラムで全てのデータが送られたか確認&再送機能が必要
                 for i, d in enumerate(struct.iter_unpack('<f', rcv_data)):
                     _data[i % self.channels].append(d[0])
                 # _data = np.array([d[0] for d in struct.iter_unpack('<f', rcv_data)])
