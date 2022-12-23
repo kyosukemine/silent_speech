17fdbd9e5455b084337d1f76d8cf2ff317866b68
kyosukemine <k3.mine.0131@gmail.com>
2022-12-23 21:06:57 +0900

    to load state paralell gpu modell

--- make_vocoder_trainset.py
+++ make_vocoder_trainset.py
@@ -18,6 +18,14 @@ from absl import flags
 FLAGS = flags.FLAGS
 flags.DEFINE_string('model', None, 'checkpoint of model to run')
 
+from collections import OrderedDict
+def fix_key(state_dict):
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        if k.startswith('module.'):
+            k = k[7:]
+        new_state_dict[k] = v
+    return new_state_dict
 
 def main():
     trainset = EMGDataset(dev=False, test=False)
@@ -26,8 +34,8 @@ def main():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     n_phones = len(phoneme_inventory)
-    model = Model(devset.num_features, devset.num_speech_features, n_phones, devset.num_sessions).to(device)
-    state_dict = torch.load(FLAGS.model)
+    model = Model(devset.num_features, devset.num_speech_features, n_phones).to(device)
+    state_dict = fix_key(torch.load(FLAGS.model))
     model.load_state_dict(state_dict)
 
     os.makedirs(os.path.join(FLAGS.output_directory, 'mels'), exist_ok=True)
